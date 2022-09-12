ce10ccdd1dbf02e68c3dfb7f8bd70f8a0a8f1a3d
Mrrohan23 <rohanpadhan85@gmail.com>
2022-09-12 12:02:55 +0530

    rohan

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'rohan'
+app.config['MYSQL_DATABASE_DB'] = 'rohans'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
